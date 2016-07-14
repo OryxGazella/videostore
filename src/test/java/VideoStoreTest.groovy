51fed719d175f88feb8053ba1aaa57506bbab713
Frank Smith <oryx.gazella@gmail.com>
2016-07-15 00:32:04 +0200

    Kill semicolons

--- src/test/java/VideoStoreTest.groovy
+++ src/test/java/VideoStoreTest.groovy
@@ -1,65 +1,65 @@
-import junit.framework.TestCase;
+import junit.framework.TestCase
 
 class VideoStoreTest extends TestCase {
-    private RentalStatement statement;
-    private Movie newRelease1;
-    private Movie newRelease2;
-    private Movie childrens;
-    private Movie regular1;
-    private Movie regular2;
-    private Movie regular3;
+    private RentalStatement statement
+    private Movie newRelease1
+    private Movie newRelease2
+    private Movie childrens
+    private Movie regular1
+    private Movie regular2
+    private Movie regular3
 
     VideoStoreTest(String name) {
-        super(name);
+        super(name)
     }
 
     protected void setUp() {
-        statement = new RentalStatement("Customer Name");
-        newRelease1 = new NewReleaseMovie("New Release 1");
-        newRelease2 = new NewReleaseMovie("New Release 2");
-        childrens = new ChildrensMovie("Childrens");
-        regular1 = new RegularMovie("Regular 1");
-        regular2 = new RegularMovie("Regular 2");
-        regular3 = new RegularMovie("Regular 3");
+        statement = new RentalStatement("Customer Name")
+        newRelease1 = new NewReleaseMovie("New Release 1")
+        newRelease2 = new NewReleaseMovie("New Release 2")
+        childrens = new ChildrensMovie("Childrens")
+        regular1 = new RegularMovie("Regular 1")
+        regular2 = new RegularMovie("Regular 2")
+        regular3 = new RegularMovie("Regular 3")
     }
 
     private void assertAmountAndPointsForReport(double expectedAmount, int expectedPoints) {
-        assertEquals(expectedAmount, statement.getAmountOwed());
-        assertEquals(expectedPoints, statement.getFrequentRenterPoints());
+        assertEquals(expectedAmount, statement.getAmountOwed())
+        assertEquals(expectedPoints, statement.getFrequentRenterPoints())
     }
 
     void testSingleNewReleaseStatement() {
-        statement.addRental(new Rental(newRelease1, 3));
-        statement.makeRentalStatement();
-        assertAmountAndPointsForReport(9.0, 2);
+        statement.addRental(new Rental(newRelease1, 3))
+        statement.makeRentalStatement()
+        assertAmountAndPointsForReport(9.0, 2)
     }
 
     void testDualNewReleaseStatement() {
-        statement.addRental(new Rental(newRelease1, 3));
-        statement.addRental(new Rental(newRelease2, 3));
-        statement.makeRentalStatement();
-        assertAmountAndPointsForReport(18.0, 4);
+        statement.addRental(new Rental(newRelease1, 3))
+        statement.addRental(new Rental(newRelease2, 3))
+        statement.makeRentalStatement()
+        assertAmountAndPointsForReport(18.0, 4)
     }
 
     void testSingleChildrensStatement() {
-        statement.addRental(new Rental(childrens, 3));
-        statement.makeRentalStatement();
-        assertAmountAndPointsForReport(1.5, 1);
+        statement.addRental(new Rental(childrens, 3))
+        statement.makeRentalStatement()
+        assertAmountAndPointsForReport(1.5, 1)
     }
 
 
     void testMultipleRegularStatement() {
-        statement.addRental(new Rental(regular1, 1));
-        statement.addRental(new Rental(regular2, 2));
-        statement.addRental(new Rental(regular3, 3));
-        statement.makeRentalStatement();
-        assertAmountAndPointsForReport(7.5, 3);
+        statement.addRental(new Rental(regular1, 1))
+        statement.addRental(new Rental(regular2, 2))
+        statement.addRental(new Rental(regular3, 3))
+        statement.makeRentalStatement()
+        assertAmountAndPointsForReport(7.5, 3)
     }
 
     void testRentalStatementFormat() {
-        statement.addRental(new Rental(regular1, 1));
-        statement.addRental(new Rental(regular2, 2));
-        statement.addRental(new Rental(regular3, 3));
+        statement.addRental(new Rental(regular1, 1))
+        statement.addRental(new Rental(regular2, 2))
+        statement.addRental(new Rental(regular3, 3))
 
         assertEquals(
                 "Rental Record for Customer Name\n" +
@@ -68,6 +68,6 @@ class VideoStoreTest extends TestCase {
                         "\tRegular 3\t3.5\n" +
                         "You owed 7.5\n" +
                         "You earned 3 frequent renter points\n",
-                statement.makeRentalStatement());
+                statement.makeRentalStatement())
     }
 }
