f1f7c1ad520b0f5f56ee8edf6bd2fbf621360105
Frank Smith <oryx.gazella@gmail.com>
2016-07-15 20:12:17 +0200

    Spock-Lite

--- src/test/java/VideoStoreTest.groovy
+++ src/test/java/VideoStoreTest.groovy
@@ -1,6 +1,6 @@
-import junit.framework.TestCase
+import spock.lang.Specification
 
-class VideoStoreTest extends TestCase {
+class VideoStoreTest extends Specification {
     private RentalStatement statement
     private Movie newRelease1
     private Movie newRelease2
@@ -9,11 +9,7 @@ class VideoStoreTest extends TestCase {
     private Movie regular2
     private Movie regular3
 
-    VideoStoreTest(String name) {
-        super(name)
-    }
-
-    protected void setUp() {
+    def setup() {
         statement = new RentalStatement("Customer Name")
         newRelease1 = new NewReleaseMovie("New Release 1")
         newRelease2 = new NewReleaseMovie("New Release 2")
@@ -23,52 +19,56 @@ class VideoStoreTest extends TestCase {
         regular3 = new RegularMovie("Regular 3")
     }
 
-
-    void testSingleNewReleaseStatement() {
+    def 'New releases cost £3 / day and earn 2 frequent renter points'() {
+        expect:
         statement.addRental(new Rental(newRelease1, 3))
         statement.makeRentalStatement()
-        assert statement.amountOwed == 9.0 as double
-        assert statement.frequentRenterPoints == 2 as int
+        statement.amountOwed == 9.0 as double
+        statement.frequentRenterPoints == 2 as int
     }
 
-    void testDualNewReleaseStatement() {
+    def 'Two new releases rented for 3 days each cost 2 x 3 x £3 = £18 earning 4 points'() {
+        expect:
         statement.addRental(new Rental(newRelease1, 3))
         statement.addRental(new Rental(newRelease2, 3))
         statement.makeRentalStatement()
-        assert statement.amountOwed == 18.0 as double
-        assert statement.frequentRenterPoints == 4 as int
+        statement.amountOwed == 18.0 as double
+        statement.frequentRenterPoints == 4 as int
     }
 
-    void testSingleChildrensStatement() {
+    def "A children's rental costs 50p a day, but only earns you a single point"() {
+        expect:
         statement.addRental(new Rental(childrens, 3))
         statement.makeRentalStatement()
-        assert statement.amountOwed == 1.5 as double
-        assert statement.frequentRenterPoints == 1 as int
+        statement.amountOwed == 1.5 as double
+        statement.frequentRenterPoints == 1 as int
     }
 
 
-    void testMultipleRegularStatement() {
+    def 'Regular movies cost £2 for two days and £1 50p every day after that'() {
+        expect:
         statement.addRental(new Rental(regular1, 1))
         statement.addRental(new Rental(regular2, 2))
         statement.addRental(new Rental(regular3, 3))
         statement.makeRentalStatement()
-        assert statement.amountOwed == 7.5 as double
-        assert statement.frequentRenterPoints == 3 as int
+        statement.amountOwed == 7.5 as double
+        statement.frequentRenterPoints == 3 as int
     }
 
-    void testRentalStatementFormat() {
+    def 'Rental statements show the cost of every movie rental, the total owned and the frequent renter points'() {
+        expect:
         statement.addRental(new Rental(regular1, 1))
         statement.addRental(new Rental(regular2, 2))
         statement.addRental(new Rental(regular3, 3))
 
-        assert statement.makeRentalStatement() ==
-                """\
-                Rental Record for Customer Name
-                \tRegular 1\t2.0
-                \tRegular 2\t2.0
-                \tRegular 3\t3.5
-                You owed 7.5
-                You earned 3 frequent renter points
-                """.stripIndent()
+        statement.makeRentalStatement() ==
+            """\
+            Rental Record for Customer Name
+            \tRegular 1\t2.0
+            \tRegular 2\t2.0
+            \tRegular 3\t3.5
+            You owed 7.5
+            You earned 3 frequent renter points
+            """.stripIndent()
     }
 }
